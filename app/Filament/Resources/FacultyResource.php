2e4a31d4826fe49731efb4be1dece402efe482e0
Kamal Maurya <kamal38005@gmail.com>
2024-12-10 18:54:08 +0530

    add facilities

---
+++ app/Filament/Resources/FacultyResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FacultyResource\Pages;
+use App\Filament\Resources\FacultyResource\RelationManagers;
+use App\Models\Faculty;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FacultyResource extends Resource
+{
+    protected static ?string $model = Faculty::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Name'),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->unique(Faculty::class, 'email')
+                    ->label('Email'),
+                Forms\Components\TextInput::make('department')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Department'),
+                Forms\Components\TextInput::make('post')
+                    ->nullable()
+                    ->maxLength(255)
+                    ->label('Post'),
+                Forms\Components\TextInput::make('phone')
+                    ->nullable()
+                    ->maxLength(255)
+                    ->label('Phone'),
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->required()
+                    ->label('Image'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Name'),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('department')
+                    ->label('Department')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('post')
+                    ->label('Post')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->label('Phone'),
+                Tables\Columns\ImageColumn::make('image')
+                    ->label('Image'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime(),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFaculties::route('/'),
+            'create' => Pages\CreateFaculty::route('/create'),
+            'edit' => Pages\EditFaculty::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -23,7 +23,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
+            ->default()->brandName('ClubNest')
             ->id('admin')
             ->path('admin')
             ->login()
